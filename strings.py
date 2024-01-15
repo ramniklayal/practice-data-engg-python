549183cae77e5ae9c031cf55cce4d9b33ca99a05
Ramnik Layal <ramniklayal@outlook.com>
2024-01-14 19:39:32 -0500

    Add variables.py and strings.py

---
+++ strings.py
@@ -0,0 +1,10 @@
+phrase = "Bowling Alley"
+
+# Different string functions
+
+# print(phrase.lower())
+# print(phrase.upper())
+# print(phrase.upper().isupper())
+# print(len(phrase))
+# print(phrase.index("Alley"))
+print(phrase.replace("Alley", "Club"))
---
+++ variables.py
@@ -0,0 +1,12 @@
+character_nickname = "Dude"         # text
+character_firstname = "Jeffrey"     # text
+character_age = 33                  # number
+is_male = True                      # boolean
+# These 3 types of data are most useful for beginners.
+# First two are strings (str), then number (int & float are common ones).
+# Third one is boolean, only has True or False values.
+
+print("There was once a man named " + character_nickname + ", ")
+print("he was " + str(character_age) + " years old.")
+print("He was called " + character_firstname + " by other people, ")
+print("but, he didn't like his real name.")
